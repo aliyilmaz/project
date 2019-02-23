26dd6aab2117b3e66fc134912694bdbcee709257
aliyilmaz <aliyilmaz.work@gmail.com>
2019-02-24 02:19:47 +0300

    Slash problem solved.

--- Mind.php
+++ Mind.php
@@ -1493,7 +1493,7 @@ class Mind {
                 $uri .='/'.implode('/', $params);
             }
 
-            if($request == $uri OR trim($request, '/') == $uri){
+            if($request == $uri OR trim($request, '/') == trim($uri, '/')){
                 $this->error_status = false;
                 $this->mindload($file, $cache);
                 exit();
