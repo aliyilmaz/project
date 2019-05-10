8277616c7a4c1ec62c19416c1924839ab66013cc
aliyilmaz <aliyilmaz.work@gmail.com>
2019-05-10 19:53:28 +0300

    2.3.6 sürümüne yükseltildi.

--- Mind.php
+++ Mind.php
@@ -3,7 +3,7 @@
 /**
  *
  * @package    Mind
- * @version    Release: 2.3.5
+ * @version    Release: 2.3.6
  * @license    GPLv3
  * @author     Ali YILMAZ <aliyilmaz.work@gmail.com>
  * @category   Php Framework, Design pattern builder for PHP.
@@ -1168,7 +1168,9 @@ class Mind {
         if(empty($url)){
             $url = $this->baseurl;
         } else {
-            $url = $this->baseurl.$url;
+            if(!$this->is_url($url)){
+                $url = $this->baseurl.$url;
+            } 
         }
 
         header('Location: '.$url);
