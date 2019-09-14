b05e9cde31f4c3ec7da4642f11575cb11dcabb1d
aliyilmaz <aliyilmaz.work@gmail.com>
2019-09-14 23:55:57 +0300

    Versiyon yükseltildi.

--- app/views/welcome.php
+++ app/views/welcome.php
@@ -72,7 +72,7 @@
 <div class="flex-center position-ref full-height">
         <div class="content">
         <div class="title m-b-md">
-            Mind <sup>3.0.5</sup>
+            Mind <sup>3.0.6</sup>
         </div>
 
         <div class="links">
