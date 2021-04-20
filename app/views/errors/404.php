ce0b0c61b6a20d486df1cb68c141def242c51ab7
aliyilmaz <aliyilmaz.work@gmail.com>
2021-04-20 22:37:03 +0300

    Google font dependency removed.

--- app/views/errors/404.php
+++ app/views/errors/404.php
@@ -7,16 +7,12 @@
 
     <title>Not Found</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <style>
         html, body {
             background-color: #fff;
             color: #636b6f;
-            font-family: 'Nunito', sans-serif;
+            font-family: Arial, Helvetica, sans-serif;
             font-weight: 100;
             height: 100vh;
             margin: 0;
